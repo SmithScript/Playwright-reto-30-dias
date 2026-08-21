27f44881c165545cd2936bedc20992066e1030c6
Brayan Smith Bernal <brayan.bernal@siigo.com>
2026-08-20 23:12:55 -0500

    Reto 1.1 superado y corregido

--- tests/LoginError.spec.ts
+++ tests/LoginError.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test'
 
-test('Login to hrm',async({page})=>{
+test('Login Error to hrm',async({page})=>{
     
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     await page.getByRole('textbox', {name: 'Username'}).fill('Admin')
